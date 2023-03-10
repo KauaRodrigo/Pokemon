f2c73ed21c6e3737771776e29c94e7b7f91dcb54
KauaRodrigo <kaurodrigo.joenck2@gmail.com>
2023-03-09 23:40:19 -0300

    gitignore e app.module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PokemonModule } from './pokemon/pokemon.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PokemonModule, ConfigModule.forRoot({
-    envFilePath: '../.env'
-  })],
+  imports: [PokemonModule],
   controllers: [AppController],
   providers: [AppService],
 })
